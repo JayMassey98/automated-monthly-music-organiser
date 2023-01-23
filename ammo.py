ca54437e1554f808de81a821f2d3603c446b9f9b
Jay Massey <jaymassey98@gmail.com>
2023-01-23 17:25:24 +0000

    Refactored code into smaller functions, enabling easier testing.

--- ammo.py
+++ ammo.py
@@ -24,18 +24,83 @@ from spotipy import SpotifyOAuth
 from datetime import date
 
 
-def request_most_played_songs():
-    """Use the user's credentials to pull HTML data containing their top songs.
+def abort_script(error_message='An error has occurred!'):
 
-    Jump to the 4th ol in the parsed HTML, which contains the relevant data.
-    Store the nested il elements as a list, stripping irrelevant characters.
+    error_message = error_message + ' Script aborted.\n\n'
+    sys.exit(error_message)
+
+
+def get_spotify_data():
+
+    # Set up the variables in order to request
+    scope = 'playlist-modify-public'        # NOTE: Should this be an argument?
+    username = '21js3bu3h7ixjemm7ypamzlga'  # NOTE: This should be an argument.
+    client_id = None        # NOTE: Currently hard-coded via setx on my home PC.
+    client_secret = None    # NOTE: Currently hard-coded via setx on my home PC.
+    redirect_uri = None     # NOTE: Currently hard-coded via setx on my home PC.
+
+    # Provide authentication for requested Spotify data.
+    auth_manager = SpotifyOAuth(scope=scope, username=username)
+    spotify_data = spotipy.Spotify(auth_manager=auth_manager)
+
+    return spotify_data
+
+
+def generate_playlist_date(todays_date=None):
+
+    # Determine the current date.
+    if todays_date == None:
+        todays_date = date.today()
+    month = todays_date.month
+    year = todays_date.year
+
+    # Use the above to calculate the playlist's start date and name.
+    if month == 1:
+        playlist_date = todays_date.replace(year=year-1, month=12)
+    else:
+        playlist_date = todays_date.replace(month=month-1)
+
+    return playlist_date
+
+
+def generate_playlist_name(playlist_date=None, month_length='short'):
+
+    # Catch the case where no playlist date is supplied.
+    if not playlist_date:
+        abort_script(error_message='No playlist date supplied!')
+
+    if month_length == 'short':
+        playlist_name = playlist_date.strftime('%b %Y')
+    else:
+        playlist_name = playlist_date.strftime('%B %Y')
+
+    return playlist_name
+
+
+def assert_playlist_does_not_exist(playlist_name='', spotify_data=None):
+
+    # Initially assume this playlist does not exist.
+    playlist_exists = False
+
+    # Catch the case where no spotify data is supplied.
+    if not spotify_data:
+        abort_script(error_message='No Spotify data supplied!')
     
-    Returns:
-        The user's top 50 (or less) most listened to songs of the past month.
+    # Extract the user's existing playlist's into a list.
+    dictionary_of_playlist_data = spotify_data.current_user_playlists().items()
+    list_of_users_playlists = spotify_data.current_user_playlists()['items']
+    playlists_total = len(list_of_users_playlists)
+    playlists_left = playlists_total
 
-    Raises:
-        Exception: If the URL containing the list of songs cannot be reached.
-    """
+    # Make sure the monthly playlist does not already exist.
+    while playlists_left:
+        current_playlist = playlists_total - playlists_left
+        if playlist_name == list_of_users_playlists[current_playlist]['name']:
+            abort_script('A playlist called ' + playlist_name + ' already exists!')
+        playlists_left -= 1
+
+
+def get_most_played_songs():
 
     # Headers taken from Chrome's inspect element of favoritemusic.guru.
     headers = {
@@ -65,87 +130,84 @@ def request_most_played_songs():
     # Authenticate the user's Spotify credentials to allow the script to work.
     website_response = requests.get(url, headers=headers, cookies=cookies)
     if website_response.status_code != 200:
-        sys.exit('Failed to reach ' + url + "! Script aborted.")
+        abort_script('Failed to reach ' + url + '!')
 
     # Strip irrelevant data so that the list of songs can be sent to Spotify.
     extracted_html = BeautifulSoup(website_response.content, 'html.parser')
     list_of_ols = extracted_html.find_all('ol')
     past_month_data = 3     # NOTE: Could utilise other data in the future.
-    list_of_songs = list_of_ols[past_month_data].contents
-    list_of_songs = [song.text.replace('—', '') for song in list_of_songs]
-    list_of_songs = [song.replace('\xa0', '') for song in list_of_songs]
+    most_played_songs = list_of_ols[past_month_data].contents
+    most_played_songs = [song.text.replace('—', '') for song in most_played_songs]
+    most_played_songs = [song.replace('\xa0', '') for song in most_played_songs]
 
-    return list_of_songs
+    return most_played_songs
 
 
-def main():
-    """Authenticate the user's Spotify credentials to allow the script to work.
+def generate_playlist(most_played_songs=[], playlist_date=None, spotify_data=None):
+    
+    # Catch the case where no list of songs is supplied.
+    if not most_played_songs:
+        abort_script(error_message='No songs supplied!')
 
-    Check if a playlist exists for the past month, exiting the script if so.
-    Send the user's list of songs to the Spotify API to generate a playlist.
+    # Catch the case where no playlist date is supplied.
+    if not playlist_date:
+        abort_script(error_message='No playlist date supplied!')
+    
+    # Catch the case where no Spotify data is supplied.
+    if not spotify_data:
+        abort_script(error_message='No Spotify data supplied!')
 
-    Raises:
-        Exception: If the user already has a playlist called <month> <year>.
-    """
-
-    # TODO: Convert the following into suppliable arguments.
-    scope = 'playlist-modify-public'
+    # TODO: Replace this line with an argument.
     username = '21js3bu3h7ixjemm7ypamzlga'
-    client_id = None        # NOTE: Currently hard-coded via setx on my home PC.
-    client_secret = None    # NOTE: Currently hard-coded via setx on my home PC.
-    redirect_uri = None     # NOTE: Currently hard-coded via setx on my home PC.
 
-    # Provide authentication to allow requesting Spotify data.
-    token = SpotifyOAuth(scope=scope, username=username)
-    spotify_data = spotipy.Spotify(auth_manager = token)
-
-    # Determine the current date.
-    todays_date = date.today()
-    month = todays_date.month
-    year = todays_date.year
-
-    # Use the above to calculate the playlist's start date and name.
-    playlist_date = todays_date.replace(month=month-1)
-    if not month:
-        playlist_date = todays_date.replace(month=12)
-        playlist_date = todays_date.replace(year=year-1)
-    playlist_name = playlist_date.strftime('%b %Y')
-
-    # Extract the user's existing playlists into a list.
-    dictionary_of_playlist_data = spotify_data.current_user_playlists().items()
-    list_of_users_playlists = list(dictionary_of_playlist_data)[1][1]
-    playlists_total = len(list_of_users_playlists)
-    playlists_left = playlists_total
-
-    # Make sure the monthly playlist does not already exist.
-    while playlists_left:
-        current_playlist = playlists_total - playlists_left
-        if playlist_name == list_of_users_playlists[current_playlist]['name']:
-            sys.exit('A playlist called ' + playlist_name + ' already exists! Aborting script.')
-        playlists_left -= 1
-
-    # Number of songs saved in-case less than 50 songs are supplied.
-    list_of_songs = request_most_played_songs()
-    songs_total = len(list_of_songs)
+    # Number of songs saved, in-case less than 50 songs are supplied.
+    songs_total = len(most_played_songs)
     songs_left = songs_total
      
-    # Auto-generate the description of the playlist.
-    playlist_description = 'My top 50 most played songs of ' + playlist_date.strftime('%B %Y') + '. Auto-generated with A.M.M.O. Visit https://github.com/JayMassey98 for more information.'
-    spotify_data.user_playlist_create(user=username,name=playlist_name,public=True,description=playlist_description)
+    # Determine the start of the playlist's description.
+    if songs_total == 1:
+        playlist_description = 'My top most played song of '
+    else:
+        playlist_description = 'My top ' + str(songs_total) + ' most played songs of '
+
+    # Get both the short and long versions of the playlist name.
+    playlist_name_short = generate_playlist_name(playlist_date=playlist_date, month_length='short')
+    playlist_name_long = generate_playlist_name(playlist_date=playlist_date, month_length='long')
+
+    # Auto-generate and update the playlist's description.
+    playlist_description = playlist_description + playlist_name_long + '. Auto-generated with A.M.M.O. Visit https://github.com/JayMassey98 for more information.'
+    spotify_data.user_playlist_create(user=username,name=playlist_name_short,public=True,description=playlist_description)
 
     # Continue looping until all songs have been appended.
     while songs_left:
         current_song = songs_total - songs_left
-        result = spotify_data.search(q=list_of_songs[current_song])
-        list_of_songs[current_song] = result['tracks']['items'][0]['uri']
+
+        # TODO: Relocate this line to its own separate function.
+        # This can break the code if certain characters are used.
+        # Extra unit testing will then determine how to fix this.
+        result = spotify_data.search(q=most_played_songs[current_song])
+
+        most_played_songs[current_song] = result['tracks']['items'][0]['uri']
         songs_left -= 1;
 
-    # Get the generated playlist.
+    # Add the determined songs into the generated playlist.
     playlist = spotify_data.user_playlists(user=username)['items'][0]['id']
+    spotify_data.user_playlist_add_tracks(user=username,playlist_id=playlist,tracks=most_played_songs)
 
-    # Add the list of songs.
-    if len(list_of_songs): # Prevents a null list error.
-        spotify_data.user_playlist_add_tracks(user=username,playlist_id=playlist,tracks=list_of_songs)
+
+def main():
+        
+    # Get the playlist requirements.
+    spotify_data = get_spotify_data()
+    playlist_date = generate_playlist_date()
+    playlist_name = generate_playlist_name(playlist_date=playlist_date, month_length='long')
+
+    # Stops the script if the playlist already exists.
+    assert_playlist_does_not_exist(playlist_name=playlist_name, spotify_data=spotify_data)
+
+    # Generate the playlist with your most played songs.
+    most_played_songs = get_most_played_songs()
+    generate_playlist(most_played_songs=most_played_songs, playlist_date=playlist_date, spotify_data=spotify_data)
 
 
 # Only runs if called directly.
